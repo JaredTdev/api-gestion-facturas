185ae9c6956967b5672a05169a9b041ebdf8bdeb
Jose Ramos <jared.ra.dev17@gmail.com>
2026-09-04 13:33:36 -0600

    Se actualizo SecurityFilterChain con metodos no deprecados.

--- src/main/java/com/api/gestion/security/SecurityConfig.java
+++ src/main/java/com/api/gestion/security/SecurityConfig.java
@@ -3,13 +3,17 @@ package com.api.gestion.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
 import com.api.gestion.security.jwt.JwtFilter;
@@ -30,12 +34,23 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    protected SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
-        httpSecurity.cors().configurationSource(request -> CorsConfiguration.app)
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(request -> {
+                request.requestMatchers("/user/login","/user/signup","/user/forgotPassword")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+            })
+            .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        httpSecurity.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
     }
 
     @Bean 
-    public AuthenticationManager AuthenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration authenticationConfiguration) throws Exception{
         return authenticationConfiguration.getAuthenticationManager();
     }
     
