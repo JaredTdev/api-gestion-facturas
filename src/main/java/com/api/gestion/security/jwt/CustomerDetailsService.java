75d585269efc8ec84597b8ab1c68248457910360
Jose Ramos <jared.ra.dev17@gmail.com>
2026-09-04 02:13:53 -0600

    Se realiza clase JwtFilter para realizar la validacion de token por usuario completo.

---
+++ src/main/java/com/api/gestion/security/jwt/CustomerDetailsService.java
@@ -0,0 +1,40 @@
+package com.api.gestion.security.jwt;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.api.gestion.dao.UserDAO;
+import com.api.gestion.pojo.User;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class CustomerDetailsService implements UserDetailsService{
+    
+    @Autowired
+    private UserDAO userDAO;
+
+    private User userDetail;
+
+    // Buscar al usuario por email
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("Dentro de loadUserByUsername {}", username);
+        userDetail = userDAO.findByEmail(username);
+        if (!Objects.isNull(userDetail)) {
+            return new org.springframework.security.core.userdetails.User(userDetail.getEmail(), 
+                        userDetail.getPassword(), new ArrayList<>());
+        }
+        else {
+            throw new UsernameNotFoundException("Usuario no encontrado");
+        }
+    }
+    
+}
---
+++ src/main/java/com/api/gestion/security/jwt/JwtFilter.java
@@ -0,0 +1,30 @@
+package com.api.gestion.security.jwt;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/* Clase para poder validar el token generado, el usuario completo.
+A detalle
+ */
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
+    }
+    
+}
